7b114c22f13e36c081975cc051ffe292b7df9f01
Federico Muzzo <federico.muzzo@immobiliare.it>
2019-11-07 17:22:44 +0100

    Update frasi.js

--- components/frasi.js
+++ components/frasi.js
@@ -7,15 +7,21 @@ const domande = [
 
 const risposte = [
     "A pranzo.",
-    "...",
-    "Sta dal team interno.",
-    "In riunione.",
-    "Con un cliente.",
     "L'abbiamo visto entrare in una cabina telefonica, ma poi è uscito Superman.",
     "Ha preso un taxi urlando ‹Segua quella macchina!›",
     "Ha risposto al transponder e Scotty l'ha portato su.",
     "Si è imbarcato in una missione quinquennale alla ricerca di nuovi mondi.",
-    "Potrei dirtelo, ma poi dovrei ucciderti."
+    "Potrei dirtelo, ma poi dovrei ucciderti.",
+    "E' appena arrivata la lettera da Hogwarts.",
+    "Ha avuto una rivelazione sulla via di Damasco.",
+    "E la cremeria?",
+    "Eh, la crisi.",
+    "Fin dove si lavano la faccia i pelati?",
+    "Alla fine dell'arcobaleno.",
+    "Il vero Sergio è sempre stato nei nostri cuori.",
+    "Nella terra dei cachi.",
+    "Guarda dentro di te e troverai la risposta.",
+    "Usa la forza, Luke."
 ];
 
 export { domande, risposte };
